8f2e7f9af31e58627fd4b55d693c7cb326bff6db
SketchTurnerrr <nareknarek45@gmail.com>
2024-10-22 11:08:44 +0300

    auth 5/?

--- convex/prompts.ts
+++ convex/prompts.ts
@@ -45,19 +45,23 @@ export const deletePrompt = mutation({
 // Updated query to fetch prompts for a user
 export const getUserPrompts = query({
   handler: async (ctx) => {
-    console.log('server identity', await ctx.auth.getUserIdentity());
+    console.log('server identity', await ctx.auth.getUserIdentity()); // convex logs return null
     const identity = await ctx.auth.getUserIdentity();
     if (!identity) {
       throw new Error('Unauthenticated');
     }
 
-    console.log('identity :', identity);
+    // same here
+    // const userId = await getAuthUserId(ctx);
+    // if (userId === null) {
+    //   throw new Error('Client is not authenticated!');
+    // }
+    // const user = await ctx.db.get(userId);
 
     const prompts = await ctx.db
       .query('prompts')
       .filter((q) => q.eq(q.field('userId'), identity.subject.split('|')[0]))
       .collect();
-    console.log('prompts :', prompts);
 
     return prompts;
   },
